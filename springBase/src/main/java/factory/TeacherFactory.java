540017bf894b4c9abc75658a13dc50e511d3ecc3
zhangj <258523454@qq.com>
2019-01-22 16:46:52 +0800

    动态工厂，静态工厂

---
+++ springBase/src/main/java/factory/TeacherFactory.java
@@ -0,0 +1,23 @@
+package factory;
+
+import entity.Teacher;
+
+/**
+ * Created By
+ *
+ * @author :   zhangj
+ * @date :   2019-01-22
+ */
+public class TeacherFactory {
+    public Teacher createTeacher() {
+        Teacher teacher = new Teacher();
+        System.out.println("[动态工厂]TeacherFactory 负责创建 Teacher 实例对象！");
+        return teacher;
+    }
+
+    public static Teacher createTeacherStatic() {
+        Teacher teacher = new Teacher();
+        System.out.println("[静态工厂]TeacherFactory 负责创建 Teacher 实例对象！");
+        return teacher;
+    }
+}
---
+++ springBase/src/test/factory/TeacherFactoryTest.java
@@ -0,0 +1,28 @@
+package factory;
+
+import com.alibaba.fastjson.JSONObject;
+import entity.Teacher;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created By
+ *
+ * @author :   zhangj
+ * @date :   2019-01-22
+ */
+public class TeacherFactoryTest {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring/spring-factory.xml");
+
+        // 动态工厂:高并发的情况下会占用较多的内存
+        Teacher teacher1 = (Teacher) applicationContext.getBean("teacher1");
+        System.out.println(teacher1 + ":" + JSONObject.toJSONString(teacher1));
+
+        // 静态工厂
+        Teacher teacher2 = (Teacher) applicationContext.getBean("teacher2");
+        System.out.println(teacher2 + ":" + JSONObject.toJSONString(teacher2));
+    }
+
+}
